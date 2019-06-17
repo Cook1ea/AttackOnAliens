755add261c95248c652ea418df840ee68b047526
Anqi Qu <anqi.qu.eea@gmail.com>
2019-06-17 15:58:50 +0900

    设置背景颜色

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,12 +5,18 @@ import pygame
 def run_game():
     #  初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((1200, 600))
     pygame.display.set_caption("Alien Invasion")
     
+    # 背景色
+    bg_color = (230, 230, 230)
+
     # 开始游戏主循环
     while True:
         
+        # 给背景上色
+        screen.fill(bg_color)
+
         # 监视鼠标和键盘事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
