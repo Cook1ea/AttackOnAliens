9d5e8824f6b2fc7d47bf2425f21c24e9afac7fec
Anqi Qu <anqi.qu.eea@gmail.com>
2019-06-18 21:59:23 +0900

    调整了速度和界面大小

--- settings.py
+++ settings.py
@@ -4,11 +4,11 @@ class Settings():
     def __init__(self):
         # 初始化游戏设置
         # 屏幕设置
-        self.screen_width = 1200
-        self.screen_height = 600
+        self.screen_width = 2400
+        self.screen_height = 1200
         self.bg_color = (230, 230, 230)
 
         # 飞船的设置
-        self.ship_speed_factor = 2.5
+        self.ship_speed_factor = 7.5
 
 
